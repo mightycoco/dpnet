d026657dc88594f8fe7bab1a2c628471c9481e9f
mightycoco <mightycoco@outlook.com>
2017-01-30 10:22:49 +0100

    Improve usage statistics. Limit for one week.

--- src/View/Cell/EventsCell.php
+++ src/View/Cell/EventsCell.php
@@ -26,7 +26,15 @@ class EventsCell extends Cell
         $connection = ConnectionManager::get('default');
         
         $usage_created = $connection->execute(
-		    'SELECT count(created) as sum, DATE(created) as date FROM events GROUP BY date'
+		    'SELECT * FROM (SELECT COUNT(created) AS sum, DATE(created) AS date FROM events GROUP BY date DESC LIMIT 7) AS `table` ORDER BY date ASC'
+		)->fetchAll('assoc');
+        
+        $usage_created_sum = $connection->execute(
+		    'SELECT SUM(sum) as sum FROM (SELECT COUNT(created) AS sum, DATE(created) AS date FROM events GROUP BY date DESC LIMIT 7) AS `table` ORDER BY date ASC'
+		)->fetchAll('assoc');
+
+        $usage_created_max = $connection->execute(
+		    'SELECT MAX(sum) as max FROM (SELECT COUNT(created) AS sum, DATE(created) AS date FROM events GROUP BY date DESC LIMIT 7) AS `table` ORDER BY date ASC'
 		)->fetchAll('assoc');
 
         $approved = $this->Events->find('all')->where(['event_approval'=>'approved']);
@@ -39,6 +47,8 @@ class EventsCell extends Cell
         $this->set('sum', $sum->count());
         
         $this->set('usage_created', $usage_created);
+        $this->set('usage_sum', $usage_created_sum[0]['sum']);
+        $this->set('usage_max', $usage_created_max[0]['max']);
     }
     
     public function uriForm() {
